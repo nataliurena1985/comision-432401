ebb5af93b3e87b428af157b23284e80bf099bdaa
claudia <claudianataliurena@gmail.com>
2024-01-11 20:17:33 -0300

    cambiandtamaño de componentes

--- src/components/common/productCard/ProductCart.jsx
+++ src/components/common/productCard/ProductCart.jsx
@@ -6,47 +6,50 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { Col } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 function ProductCart({ elemento }) {
   return (
-    <Card sx={{ width: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={elemento.img}
-        // title={elemento.title}
-        title="hola"
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          textAlign={"center"}
-        >
-          {elemento.title}
-        </Typography>
-
-        <Typography variant="subtitle1" textAlign={"center"}>
-          ${elemento.price}.-
-        </Typography>
-
-        <Typography variant="body2" color="text.secondary">
-          {elemento.description}
-        </Typography>
-      </CardContent>
-      <CardActions style={{ display: "flex", justifyContent: "center" }}>
-        <Link to={`/itemDetail/${elemento.id}`}>
-          <Button
-            variant="contained"
-            size="small"
-            sx={{ textTransform: "none" }}
+    <Col>
+      <Card sx={{ width: 450 }}>
+        <CardMedia
+          sx={{ height: 300 }}
+          image={elemento.img}
+          // title={elemento.title}
+          title="hola"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            textAlign={"center"}
           >
-            Ver detalle
-          </Button>
-        </Link>
-      </CardActions>
-    </Card>
+            {elemento.title}
+          </Typography>
+
+          <Typography variant="subtitle1" textAlign={"center"}>
+            ${elemento.price}.-
+          </Typography>
+
+          <Typography variant="body2" color="text.secondary">
+            {elemento.description}
+          </Typography>
+        </CardContent>
+        <CardActions style={{ display: "flex", justifyContent: "center" }}>
+          <Link to={`/itemDetail/${elemento.id}`}>
+            <Button
+              variant="contained"
+              size="small"
+              sx={{ textTransform: "none" }}
+            >
+              Ver detalle
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </Col>
   );
 }
 
